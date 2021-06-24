f6259f579490bf6fb7c2827f47be94928982e551
Thomas Oliver <1505576515@mil>
2021-06-23 21:50:34 -1000

    Streamlined for scenario runs

--- Tom_Plotting.R
+++ Tom_Plotting.R
@@ -1,4 +1,6 @@
 #Tom_Plotting
+library(ggplot2)
+library(plyr)
 head(PatchLevel)
 
 #Get to clean Area Surveyed 
@@ -7,7 +9,13 @@ juv <- read.csv("JuvProject_temporal_SITE.csv")
 
 jmhi=subset(juv,REGION=="MHI"&GENUS_CODE%in%c("POCS","MOSP","POSP"))
 
-library(ggplot2)
+ddply(jmhi,.(GENUS_CODE),summarize,
+      q05=quantile(113*JuvColDen,.05),
+      q25=quantile(113*JuvColDen,.25),
+      q50=quantile(113*JuvColDen,.50),
+      q75=quantile(113*JuvColDen,.75),
+      q95=quantile(113*JuvColDen,.95))
+
 ggplot(jmhi,aes(x=JuvColDen))+
   geom_histogram()+
   scale_x_log10()+
