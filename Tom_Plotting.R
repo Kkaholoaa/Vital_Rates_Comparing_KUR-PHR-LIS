ce2c33fa00223cf61f780f6bdd4ab23963f769bc
Thomas Oliver <1505576515@mil>
2021-06-25 11:40:45 -1000

    Friday morning commit

--- Tom_Plotting.R
+++ Tom_Plotting.R
@@ -4,10 +4,14 @@ library(plyr)
 head(PatchLevel)
 
 #Get to clean Area Surveyed 
-setwd("T:/Benthic/Projects/Juvenile Project") # set working directory 
-juv <- read.csv("JuvProject_temporal_SITE.csv")
-
+juv <- read.csv("T:/Benthic/Projects/Juvenile Project/JuvProject_temporal_SITE.csv")
+cov=read.csv("T:/Benthic/Data/REA Coral Demography & Cover/Summary Data/Sector/BenthicCover_2010-2019_Tier3_SECTOR_forTimeseries.csv")
 jmhi=subset(juv,REGION=="MHI"&GENUS_CODE%in%c("POCS","MOSP","POSP"))
+cmhi=subset(cov,REGION=="MHI")[,c("REGION", "ISLAND", "ANALYSIS_SEC", "ANALYSIS_YEAR",  "N",
+                                  "Mean.POCS","PooledSE.POCS",
+                                  "Mean.POMA","PooledSE.POCS"
+                                  "Mean.POEN","PooledSE.POCS")]#&GENUS_CODE%in%c("POCS","MOSP","POSP"))
+head(cmhi)
 
 ddply(jmhi,.(GENUS_CODE),summarize,
       q05=quantile(113*JuvColDen,.05),
@@ -20,5 +24,7 @@ ggplot(jmhi,aes(x=JuvColDen))+
   geom_histogram()+
   scale_x_log10()+
   facet_grid(GENUS_CODE~OBS_YEAR)+
-  geom_vline(xintercept = 35.75/113,color="red")
+  geom_vline(xintercept = 35.75*.1,color="red")+
+  geom_vline(xintercept = 30.75*.1,color="red")
+
 
